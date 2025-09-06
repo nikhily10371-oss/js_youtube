f438e64cc1e6a227d9a75bb8d26e1dd7b687d751
nikhily10371-oss <nikhily10371@gmail.com>
2025-09-06 09:56:23 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,9 +22,36 @@
 //"nikhil"=>true
 
 
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
-let someNumber=33
-let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
 
+
+//********************Oprations******************//
+// let value=3
+// let negvalue=-value
+// console.log(negvalue)
+
+
+
+// let str1="Nikhil"
+// let str2="yadav"
+// let str3=str1+str2
+// console.log(str3)
+
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+
+// console.log(true)=>true
+// console.log(+true)=>1
+// console.log(+"")=>0
+
+let gamecounter=100
+gamecounter++;
+console.log(gamecounter)
